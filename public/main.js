eb30b5c65a4a04cda0b979ec0aae8b499f4014bf
dithiane <elenamena.colorado@gmail.com>
2023-05-10 16:10:21 -0600

    Added server parametr

--- public/main.js
+++ public/main.js
@@ -2,13 +2,13 @@ const catBtn = document.querySelector('#cat')
 const dogBtn = document.querySelector('#dog')
 
 const getCat = () => {
-    axios.get('http://localhost:4000/api/cat')
+    axios.get('http://[server]:4000/api/cat')
         .then(res => alert(res.data))
         .catch(err => console.log(err))
 }
 
 const getDog = () => {
-    axios.get('http://localhost:4000/api/dog')
+    axios.get('http://[server]:4000/api/dog')
         .then(res => alert(res.data))
         .catch(err => console.log(err))
 }
