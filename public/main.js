c251ab4ae031c6f73e61454008e24196f797c03c
dithiane <elenamena.colorado@gmail.com>
2023-05-10 13:36:26 -0600

    Refactoring

--- public/main.js
+++ public/main.js
@@ -4,11 +4,13 @@ const dogBtn = document.querySelector('#dog')
 const getCat = () => {
     axios.get('http://localhost:4000/api/cat')
         .then(res => alert(res.data))
+        .catch(err => console.log(err))
 }
 
 const getDog = () => {
     axios.get('http://localhost:4000/api/dog')
         .then(res => alert(res.data))
+        .catch(err => console.log(err))
 }
 
 catBtn.addEventListener('click', getCat)
