559c4f3555d2b66eed043aa184eecd6bc35b4746
dithiane <elenamena.colorado@gmail.com>
2023-05-11 13:23:56 -0600

    Minor changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const app = express()
 app.use(cors())
-app.use(express.static(path.join(__dirname, "js")));
+app.use(express.static('public'))
 
 const {ROLLBAR_ACCESS_TOKEN} = process.env
 
