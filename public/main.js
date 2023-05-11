b9f1dc1b101707136b165f5c2c2c3be71d5959a1
dithiane <elenamena.colorado@gmail.com>
2023-05-11 15:45:42 -0600

    Minor changes

--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 const btn = document.querySelector('button')
-const baseUrl = 'http:/18.204.19.56/'
+const baseUrl = 'api/'
 //const baseUrl = 'http://localhost:4000'
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
 
 axios
-.get(`${baseUrl}/api/dog/?name=toby`)
+.get(`${baseUrl}dog/?name=toby`)
 .then(res => {
     console.log(res)
 })
